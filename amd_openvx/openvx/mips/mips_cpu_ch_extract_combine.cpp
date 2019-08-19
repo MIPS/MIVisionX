1ce34778c2e68bd59b454da2c2a97d9d22724bab
Milko Leporis <Milko.Leporis@rt-rk.com>
2019-08-19 11:28:53 +0200

    [MIPS] Add ChannelExtract_U8_U24_Pos1
    - Add MSA optimization
    - Add C implementation
    
    Change-Id: I88893fc15e59b5818b58d2c157bc9c4fe96958c0

--- amd_openvx/openvx/mips/mips_cpu_ch_extract_combine.cpp
+++ amd_openvx/openvx/mips/mips_cpu_ch_extract_combine.cpp
@@ -828,6 +828,78 @@ int HafCpu_ChannelExtract_U8_U24_Pos0
 	return AGO_SUCCESS;
 }
 
+int HafCpu_ChannelExtract_U8_U24_Pos1
+	(
+		vx_uint32     dstWidth,
+		vx_uint32     dstHeight,
+		vx_uint8    * pDstImage,
+		vx_uint32     dstImageStrideInBytes,
+		vx_uint8    * pSrcImage,
+		vx_uint32     srcImageStrideInBytes
+	)
+{
+	int alignedWidth = dstWidth & ~15;
+	int postfixWidth = (int) dstWidth - alignedWidth;
+
+#if ENABLE_MSA
+	v16i8 r0, r1, r2;
+	v16i8 mask1 = __builtin_msa_ld_b((void *) (((v16i8 *) &dataChannelExtract) + 7), 0);
+	v16i8 mask2 = __builtin_msa_ld_b((void *) (((v16i8 *) &dataChannelExtract) + 8), 0);
+	v16i8 mask3 = __builtin_msa_ld_b((void *) (((v16i8 *) &dataChannelExtract) + 9), 0);
+#endif
+
+	int height = (int) dstHeight;
+	while (height)
+	{
+		vx_uint8 * pLocalSrc = pSrcImage;
+		vx_uint8 * pLocalDst = pDstImage;
+		int width = alignedWidth >> 4;
+
+#if ENABLE_MSA
+		while (width)
+		{
+			r0 = __builtin_msa_ld_b((void *) pLocalSrc, 0);
+			r1 = __builtin_msa_ld_b((void *) (pLocalSrc + 16), 0);
+			r2 = __builtin_msa_ld_b((void *) (pLocalSrc + 32), 0);
+
+			r0 = __builtin_msa_vshf_b(mask1, r0, r0);
+			r1 = __builtin_msa_vshf_b(mask2, r1, r1);
+			r2 = __builtin_msa_vshf_b(mask3, r2, r2);
+
+			r0 = (v16i8) __builtin_msa_or_v((v16u8) r0, (v16u8) r1);
+			r0 = (v16i8) __builtin_msa_or_v((v16u8) r0, (v16u8) r2);
+
+			__builtin_msa_st_b(r0, (void *) pLocalDst, 0);
+
+			width--;
+			pLocalSrc += 48;
+			pLocalDst += 16;
+		}
+
+		width = postfixWidth;
+		while (width)
+		{
+			*pLocalDst++ = *++pLocalSrc;
+			pLocalSrc += 2;
+			width--;
+		}
+#else   // C
+		width = dstWidth;
+		while (width)
+		{
+			*pLocalDst++ = *++pLocalSrc;
+			pLocalSrc += 2;
+			width--;
+		}
+#endif
+		pSrcImage += srcImageStrideInBytes;
+		pDstImage += dstImageStrideInBytes;
+		height--;
+	}
+
+	return AGO_SUCCESS;
+}
+
 int HafCpu_ChannelCopy_U8_U8
 	(
 		vx_uint32     dstWidth,
